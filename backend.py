f149d0d2e2a9713a8183b03cd3b428f48b39e71e
Murtaza Hussain <murtazajii876@gmail.com>
2024-05-03 23:02:40 +0500

    Update backend.py
    Backend logic

--- backend.py
+++ backend.py
@@ -18,3 +18,34 @@ class SimpleOperator(bpy.types.Operator, BaseOperator):
     bl_label = "Tool Name"
 
 bpy.utils.register_class(SimpleOperator)
+class SimpleOperator(bpy.types.Operator):
+    """ See example above """
+
+def register():
+    bpy.utils.register_class(SimpleOperator)
+
+def unregister():
+    bpy.utils.unregister_class(SimpleOperator)
+
+if __name__ == "__main__":
+    register()
+
+
+class MyMaterialSubProps(bpy.types.PropertyGroup):
+    my_float: bpy.props.FloatProperty()
+
+class MyMaterialGroupProps(bpy.types.PropertyGroup):
+    sub_group: bpy.props.PointerProperty(type=MyMaterialSubProps)
+
+def register():
+    bpy.utils.register_class(MyMaterialSubProps)
+    bpy.utils.register_class(MyMaterialGroupProps)
+    bpy.types.Material.my_custom_props: bpy.props.PointerProperty(type=MyMaterialGroupProps)
+
+def unregister():
+    del bpy.types.Material.my_custom_props
+    bpy.utils.unregister_class(MyMaterialGroupProps)
+    bpy.utils.unregister_class(MyMaterialSubProps)
+
+if __name__ == "__main__":
+    register()
