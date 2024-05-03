3483f5663ab2482e758fa9f18e476131414ec4c2
Murtaza Hussain <murtazajii876@gmail.com>
2024-05-03 22:45:09 +0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -8,3 +8,13 @@ class SimpleOperator(bpy.types.Operator):
         return {'FINISHED'}
 
 bpy.utils.register_class(SimpleOperator)
+class BaseOperator:
+    def execute(self, context):
+        print("Hello World BaseClass")
+        return {'FINISHED'}
+
+class SimpleOperator(bpy.types.Operator, BaseOperator):
+    bl_idname = "object.simple_operator"
+    bl_label = "Tool Name"
+
+bpy.utils.register_class(SimpleOperator)
